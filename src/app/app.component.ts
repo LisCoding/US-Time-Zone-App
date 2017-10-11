c105fe834c55064a351efe5081a81c3f28728ddf
Liseth Cardozo <cardozoliseth@gmail.com>
2017-10-11 13:07:58 -0700

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  time_zone;
+  date_now;
+  time_pst(){
+    this.date_now = new Date()
+    this.time_zone = 'pst';
+  }
+  time_mst(){
+    this.date_now = new Date()
+    this.date_now.setHours(this.date_now.getHours()+ 1)
+    this.time_zone = 'mst';
+  }
+  time_cst(){
+    this.date_now = new Date()
+    this.date_now.setHours(this.date_now.getHours()+ 2)
+    this.time_zone = 'cst';
+  }
+  time_est(){
+    this.date_now = new Date()
+    this.date_now.setHours(this.date_now.getHours()+ 3)
+    this.time_zone = 'est';
+  }
+  clear(){
+  this.date_now = ""
+  this.time_zone = '';
+  }
 }
